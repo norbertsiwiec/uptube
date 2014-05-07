e7e56a86932582173ead8587952512dd1ff3b923
Norbert <norbi83@wp.pl>
2014-05-07 18:11:17 +0200

    created a rails app and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Uptube::Application.config.secret_key_base = '33bb2e1549dbefe2b11b4d29a0acd662bcc276f9b28fa8090e6e75b0f4cf762ea43971b80fcde8d2aedc92fe029c20537bc314a1d255a02708b58ba8c7e64eea'
